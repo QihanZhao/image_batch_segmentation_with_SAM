6abb437abd4880c6b5a70b815ba03f43f0dd045a
HanX-gthb <94861831+HanX-gthb@users.noreply.github.com>
2023-06-10 20:50:17 +0800

    Update image_batch_segmentation_with_SAM.py

--- image_batch_segmentation_with_SAM.py
+++ image_batch_segmentation_with_SAM.py
@@ -19,7 +19,7 @@ from segment_anything import SamAutomaticMaskGenerator, sam_model_registry
 
 sam_checkpoint = "./sam_vit_h_4b8939.pth"
 model_type = "vit_h"
-device = "cuda"
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
 sam.to(device=device)
 mask_generator = SamAutomaticMaskGenerator(sam)
